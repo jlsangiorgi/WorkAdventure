04fa77a10bf654142e799aad620f1845356d977d
Justin Sangiorgi <Justin@sangiorgi.home.local>
2022-11-18 17:05:30 -0500

    removed tsx

--- tiles/SunnySide/spr_tileset_sunnysideworld.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="spr_tileset_sunnysideworld" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../../../../../Downloads/Tiles/Sunnyside World/spr_tileset_sunnysideworld.png" width="1024" height="1024"/>
-</tileset>
